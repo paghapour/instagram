a98c657dbded48d00d408f875ad1f7db6b98df30
pooriaaghapour <pouriaaghapour@gmail.com>
2022-02-20 23:11:42 +0330

    insert

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ client = pymongo.MongoClient('mongo://localhost:27017/')
 
 mydb = client["instagram"]
 mycol = mydb["profile"]
+
+profile= [{"_userid" : "1" ,
+          "username" : "pooria" ,
+          "fullname": "pooriaaghapour" ,
+          "biography" : "example" ,
+          "total_followers" : "20" ,
+          "total_followees" : "10" ,
+          "media_count" : "3" ,
+          "update_at" : "datetime" ,
+          "create_at" : "datetime"
+          },
+          {"_userid" : "2" ,
+          "username" : "arshia" ,
+          "fullname": "arshiaaghapour" ,
+          "biography" : "example" ,
+          "total_followers" : "30" ,
+          "total_followees" : "40" ,
+          "media_count" : "7" ,
+          "update_at" : "datetime" ,
+          "create_at" : "datetime"
+          }]
+mycol.insert_many(profile)
